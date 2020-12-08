6e4341d54cec9215fcbc580fc0f741a42f26dbc6
Vinnie Lopez <vinnielo01@gmail.com>
2020-12-08 11:52:15 -0800

    completed firebase RTDB, Auto signin and signout funtions

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -1,76 +1,76 @@
-import { firebase, usersCollection } from '../../firebase';
+import { firebase, usersCollection } from "../../firebase";
 import "firebase/firestore";
 
+export const registerUser = async ({ email, password }) => {
 
+  try {
+    const response = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
 
+    const { user } = response;
+    const DB = firebase.database();
+    const userProfile = {
+      uid: user.uid,
+      email: email,
+    };
+    await DB.ref("users/" + user.uid).set(userProfile);
+    return {
+      isAuth: true,
+      user: userProfile,
+    };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
 
-export const registerUser =  async ({ email,password }) =>{
-    // firebase.auth()
-    // .createUserWithEmailAndPassword(email,password)
-    // .then( (response)=>{
-    //     const {user} = response;
- 
-    //     const userProfile = {
-    //         uid: user.uid,
-    //         email: email
-    //     };
-        
-        
-    //    db.collection('users').add(userProfile).then(function(docRef) {
-    //     console.warn("Document written with ID: ", docRef.id).catch(err=>{
-    //         if(err) throw err
-    //     });
+const readData = () => {
+  var userId = firebase.auth().currentUser.uid;
+  // console.warn(userId)
 
-      
-    // })
-            
-       
-        
-    //     return { isAuth: true, 
-    //         //         // user:userProfile
-    //              }
-       
-    // })
+  var ref = firebase.database().ref("/users/" + userId);
 
-    try{
-        const response = await firebase.auth()
-        .createUserWithEmailAndPassword(email,password);
+ref.on("value", function(snapshot) {
+  //  console.warn(snapshot.val());
+}, function (error) {
+   console.warn("Error: " + error.code);
+});
+};
 
-        const {user} = response;
-        const DB = firebase.database();
-        const userProfile = {
-            uid: user.uid,
-            email: email
-        };
-        await DB.ref("users/" + user.uid).set(userProfile)
-        return { 
-            isAuth: true, 
-            user:userProfile
-         }
-    }catch(error){
-        return { error:error.message }
-    }
-}
+export const loginUser = async ({ email, password }) => {
+  try {
+    const response = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    // once the user logs in the data on the RTDB needs to be pushed to the INITIAL_STATE.
+    // const userProfile = await usersCollection.doc(response.user.uid).get();
+    const data = readData();
+    
+    return {
+      isAuth: true,
+      user: data,
+    };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
 
-const readData = ()=>{
-    var userId = firebase.auth().currentUser.uid;
-    return firebase.database().ref('/users/' + userId).once('value')
-}
+export const autoSignIn = () => (
+  new Promise((resolve,reject)=>{
+      firebase.auth().onAuthStateChanged( user => {
+          if(user){
+            firebase.database().ref("/users/" + user.uid).get().then( snapshot =>{
+                  resolve({ isAuth: true, user: snapshot.val() })
+                  console.warn(snapshot.val())
+              })
+          } else {
+            resolve({ isAuth: false, user:[] })
+          }
+  
+      })
+  })
+)
 
-export const loginUser = async({ email,password }) =>{
-    try{
-        const response =await firebase.auth()
-        .signInWithEmailAndPassword(email,password);
-        // once the user logs in the data on the RTDB needs to be pushed to the INITIAL_STATE.
-        // const userProfile = await usersCollection.doc(response.user.uid).get();
-        const data = readData()
-         console.warn(data)
-
-        return { 
-            isAuth: true,
-             user:data 
-            }
-    }catch(error){
-        return { error:error.message }
-    }
-}
+export const logoutUser = () => (
+  firebase.auth().signOut()
+)
