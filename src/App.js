ab2f5a48646af3c496bc584c7f5cc731d75f47df
tiiedye <tiiedye@gmail.com>
2020-12-10 12:29:23 -0800

    arranging Stacks and editing Home

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ import { Colors } from './utils/tools';
 
 const Drawer = createDrawerNavigator();
 
-import { Stack, HomeStack, VideosStack, screenOptions } from './routes/stacks';
+import { Stack, HomeStack, StoriesStack, screenOptions } from './routes/stacks';
 import AuthScreen from './components/auth';
-import ProfileScreen from './components/user/profile/profile';
-import VideoScreen from './components/home/videos/video';
+import SettingsScreen from './components/user/profile/profile';
+import StoryScreen from './components/home/videos/video';
 import Splash from './components/auth/splash';
 
 
@@ -25,8 +25,8 @@ const MainDrawer = () => (
     drawerStyle={{backgroundColor: Colors.black}}
   >
     <Drawer.Screen name="Home" component={HomeStack}/>
-    <Drawer.Screen name="Videos" component={VideosStack}/>
-    <Drawer.Screen name="Profile" component={ProfileScreen}/>
+    <Drawer.Screen name="Stories" component={StoriesStack}/>
+    <Drawer.Screen name="Settings" component={SettingsScreen}/>
   </Drawer.Navigator>
 )
 
@@ -54,8 +54,8 @@ class App extends Component{
                 options={{ headerShown:false}}
               />
               <Stack.Screen 
-                name="VideoScreen" 
-                component={ VideoScreen }
+                name="StoryScreen" 
+                component={ StoryScreen }
                 options={{
                   ...screenOptions,
                   headerBackTitleVisible:false
