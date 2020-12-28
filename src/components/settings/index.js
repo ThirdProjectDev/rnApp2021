099d8d24f6d5a6c923d2b496fad637bab3a734df
AnnaxGrace <anna.grace.conover@gmail.com>
2020-12-28 13:34:35 -0600

    updated volume to be used with firestore

--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -1,83 +1,109 @@
-
-import React, { Component } from 'react';
-import Slider from "@react-native-community/slider"
-import { Picker } from "@react-native-community/picker"
-import { rain1, fire1, fire2, fire3, rain2, rain3, forest1, forest2, forest3 } from "../../utils/sounds"
-import { firebase, usersCollection, storiesCollection } from "../../firebase";
-import { playRainSound, playForestSound, playFireSound, stopFireSound, stopRainSound, stopForestSound} from "../../utils/tools"
-import PlayImage from '../../utils/playImage';
-import StopImage from '../../utils/stopImage';
-
-
+import React, { Component } from "react";
+import Slider from "@react-native-community/slider";
+import { Picker } from "@react-native-community/picker";
 import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Platform,
-} from 'react-native';
-import { connect } from 'react-redux';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+  rain1,
+  fire1,
+  fire2,
+  fire3,
+  rain2,
+  rain3,
+  forest1,
+  forest2,
+  forest3,
+} from "../../utils/sounds";
+import { firebase, usersCollection, storiesCollection } from "../../firebase";
+import {
+  showToast,
+  playRainSound,
+  playForestSound,
+  playFireSound,
+  stopFireSound,
+  stopRainSound,
+  stopForestSound,
+} from "../../utils/tools";
+import PlayImage from "../../utils/playImage";
+import StopImage from "../../utils/stopImage";
 
-//volume control
-rain1.setVolume(50);
-fire1.setVolume(.01)
-fire2.setVolume(1)
-fire3.setVolume(.25)
+import { StyleSheet, ScrollView, View, Text, Platform } from "react-native";
+import { connect } from "react-redux";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { ThemeConsumer } from "react-native-elements";
+
+// //volume control
+// rain1.setVolume(50);
+// fire1.setVolume(0.01);
+// fire2.setVolume(1);
+// fire3.setVolume(0.25);
 
 class SettingsComponent extends Component {
   state = {
-    setting: { fire: 'fire1', forest: 'forest1', rain: 'rain1', email: firebase.auth().currentUser.email, uid: firebase.auth().currentUser.uid },
-    sliderVol: .25
-  }
+    setting: {
+      fire: "fire1",
+      forest: "forest1",
+      rain: "rain1",
+      volume: .5,
+      email: firebase.auth().currentUser.email,
+      uid: firebase.auth().currentUser.uid,
+    },
+  };
 
   componentDidMount() {
-    const currentUID = firebase.auth().currentUser.uid
+    const currentUID = firebase.auth().currentUser.uid;
     usersCollection
       .doc(currentUID)
       .get()
-      .then(snapshot => {
-        const data = snapshot.data()
-        console.warn(data)
+      .then((snapshot) => {
+        const data = snapshot.data();
+        console.warn(data);
         if (data) {
-          this.setState({setting: {email: data.email, uid: data.uid, fire: data.fire, forest: data.forest, rain: data.rain }})
+          this.setState({
+            setting: {
+              email: data.email,
+              uid: data.uid,
+              fire: data.fire,
+              forest: data.forest,
+              rain: data.rain,
+              volume: data.volume
+            },
+          });
         }
       });
   }
 
-
   saveSettings() {
-    const newSettings = usersCollection.doc(this.state.setting.uid)
-    newSettings.set(this.state.setting)
+    const newSettings = usersCollection.doc(this.state.setting.uid);
+    newSettings.set(this.state.setting);
+    showToast("success", "Settings Saved!")
   }
-
-  
 
   render() {
     return (
       <>
-        <ScrollView style={{ backgroundColor: '#F0F0F0' }}>
+        <ScrollView style={{ backgroundColor: "#F0F0F0" }}>
           <View>
-            <Text
-              style={styles.title}
-            >
-              Settings
-            </Text>
-            <Text
-              style={styles.soundSelection}
-            >Rain&nbsp;
+            <Text style={styles.title}>Settings</Text>
+            <Text style={styles.soundSelection}>
+              Rain&nbsp;
               <TouchableHighlight
-                onPress={()=>playRainSound(this.state.setting.rain, this.state.sliderVol)}
-                icon={{ name:'play-circle',type:'font-awesome',color:'#fff',size:50}}
+                onPress={() =>
+                  playRainSound(this.state.setting.rain, this.state.setting.volume)
+                }
+                icon={{
+                  name: "play-circle",
+                  type: "font-awesome",
+                  color: "#fff",
+                  size: 50,
+                }}
                 size={15}
-                label={'play'}
+                label={"play"}
                 accessibilityLabel="Button to play rain sound"
               >
-                <PlayImage/>
+                <PlayImage />
               </TouchableHighlight>
-                &nbsp;
-                <TouchableHighlight
-                onPress={()=>stopRainSound()}
+              &nbsp;
+              <TouchableHighlight
+                onPress={() => stopRainSound()}
                 title="stop"
                 accessibilityLabel="Button to stop rain sound"
               >
@@ -87,57 +113,69 @@ class SettingsComponent extends Component {
 
             <Picker
               selectedValue={this.state.setting.rain}
-              style={{ height: 50, width: 150 }, styles.pickers}
+              style={({ height: 50, width: 150 }, styles.pickers)}
               onValueChange={(itemValue, itemIndex) =>
-                this.setState({setting: {...this.state.setting, rain: itemValue }})
-              }>
+                this.setState({
+                  setting: { ...this.state.setting, rain: itemValue },
+                })
+              }
+            >
               <Picker.Item label="Rain 1" value="rain1" />
               <Picker.Item label="Rain 2" value="rain2" />
               <Picker.Item label="Rain 3" value="rain3" />
               <Picker.Item label="Randomize" value="Random" />
             </Picker>
-            <Text
-              style={styles.soundSelection}>Forest&nbsp;
-            <TouchableHighlight
-                onPress={()=>playForestSound(this.state.setting.forest, this.state.sliderVol)}
+            <Text style={styles.soundSelection}>
+              Forest&nbsp;
+              <TouchableHighlight
+                onPress={() =>
+                  playForestSound(
+                    this.state.setting.forest,
+                    this.state.setting.volume
+                  )
+                }
                 size={15}
                 accessibilityLabel="Button to play forest sound"
               >
-                <PlayImage/>
+                <PlayImage />
               </TouchableHighlight>
-                &nbsp;
-                <TouchableHighlight
-                onPress={()=>stopForestSound()}
+              &nbsp;
+              <TouchableHighlight
+                onPress={() => stopForestSound()}
                 title="stop"
                 accessibilityLabel="Button to stop forest sound"
               >
                 <StopImage />
               </TouchableHighlight>
-
             </Text>
             <Picker
               selectedValue={this.state.setting.forest}
-              style={{ height: 50, width: 150 }, styles.pickers}
+              style={({ height: 50, width: 150 }, styles.pickers)}
               onValueChange={(itemValue, itemIndex) =>
-                this.setState({setting: {...this.state.setting, forest: itemValue }})
-              }>
+                this.setState({
+                  setting: { ...this.state.setting, forest: itemValue },
+                })
+              }
+            >
               <Picker.Item label="Forest 1" value="forest1" />
               <Picker.Item label="Forest 2" value="forest2" />
               <Picker.Item label="Forest 3" value="forest3" />
               <Picker.Item label="Randomize" value="Random" />
             </Picker>
-            <Text
-              style={styles.soundSelection}>Fire&nbsp;
+            <Text style={styles.soundSelection}>
+              Fire&nbsp;
               <TouchableHighlight
-                onPress={()=>playFireSound(this.state.setting.fire, this.state.sliderVol)}
+                onPress={() =>
+                  playFireSound(this.state.setting.fire, this.state.setting.volume)
+                }
                 size={15}
                 accessibilityLabel="Button to play fire sound"
               >
-                <PlayImage/>
+                <PlayImage />
               </TouchableHighlight>
-                &nbsp;
-                <TouchableHighlight
-                onPress={()=>stopFireSound()}
+              &nbsp;
+              <TouchableHighlight
+                onPress={() => stopFireSound()}
                 title="stop"
                 accessibilityLabel="Button to stop fire sound"
               >
@@ -146,9 +184,11 @@ class SettingsComponent extends Component {
             </Text>
             <Picker
               selectedValue={this.state.setting.fire}
-              style={{ height: 50, width: 150 }, styles.pickers}
+              style={({ height: 50, width: 150 }, styles.pickers)}
               onValueChange={(itemValue, itemIndex) =>
-                this.setState({setting: {...this.state.setting, fire: itemValue }})
+                this.setState({
+                  setting: { ...this.state.setting, fire: itemValue },
+                })
               }
             >
               <Picker.Item label="Fire 1" value="fire1" />
@@ -156,37 +196,37 @@ class SettingsComponent extends Component {
               <Picker.Item label="Fire 3" value="fire3" />
               <Picker.Item label="Randomize" value="Random" />
             </Picker>
-            <Text h2>
-              Volume
-            </Text>
+            <Text h2>Volume</Text>
             <Slider
               style={{ width: 200, height: 40 }}
               maximumValue={1}
               minimumValue={0}
-              step={.25}
-              value={.5}
-              onValueChange={itemValue =>
-                this.setState({ sliderVol: itemValue })}
+              step={0.25}
+              value={0.5}
+              onValueChange={(itemValue) =>
+                this.setState({ setting: {...this.state.setting, volume: itemValue }, })
+              }
             />
 
             <TouchableHighlight
               style={styles.saveBtn}
               color="#7C2717"
-              onPress={this.saveSettings.bind(this)}
+              onPress={
+                this.saveSettings.bind(this)
+
+              }
               title="stop"
               accessibilityLabel="Button to stop fire sound"
             >
-              <Text h2>
-                Save Settings
-            </Text>
-
+              <Text h2>Save Settings</Text>
             </TouchableHighlight>
 
-            {/* <Text>
+            {/* <Text
+              // style={{ display: this.state.alertMessage}}
+              // style={{ display: 'inline'}}
+            >
           Sound Credit to Zapsplat
         </Text> */}
-
-
           </View>
         </ScrollView>
       </>
@@ -198,15 +238,15 @@ const styles = StyleSheet.create({
     ...Platform.select({
       ios: {
         margin: 1,
-      }
-    })
+      },
+    }),
   },
   saveBtn: {
     backgroundColor: "#A25B2C",
     borderRadius: 20,
     padding: 10,
     elevation: 2,
-    marginLeft: '30%',
+    marginLeft: "30%",
     marginTop: 30,
     width: 110,
   },
@@ -215,12 +255,11 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 60,
-  }
+  },
 });
 function mapStateToProps(state) {
   return {
-    News: state.News
-  }
+    News: state.News,
+  };
 }
-export default connect(mapStateToProps)(SettingsComponent)
-
+export default connect(mapStateToProps)(SettingsComponent);
